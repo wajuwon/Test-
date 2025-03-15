aca5e9cb62c101b535e5e4086d5eddc8eeb6e279
Ayomide Afolabi <aafolabi@smartterm.io>
2025-03-15 04:28:07 +0100

    Secont commit

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-alert('hello world!')
+const day = "saturday"
 
-const name = "Tester"
+console.log(day)
 
-alert(name)
+const position = 7
+
+console.log(position)
+
+const output = 'today is ' + 'saturday' + '. It is the ' + 7 +'th the day of the week.'
+
+console.log(output)
