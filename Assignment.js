11310654c2cdcead56dc4634452bb6f0344e4cc5
Ayomide Afolabi <aafolabi@smartterm.io>
2025-03-22 12:50:07 +0100

    assignmentn

---
+++ Assignment.js
@@ -0,0 +1,25 @@
+let number = 8
+
+if (number % 2===0 ) {
+  console.log (number  + ' is even ' )
+}
+
+else {
+  console.log ( number + ' is odd' )  
+}
+
+
+let side1 = 3
+let side2 = 2
+let side3 = 5
+
+if (side1 === side2 && side2=== side3 && side1 === side3 ) {
+console.log ('Equilateral triangle')
+}
+else if (side1 === side2 || side2 === side3 || side1 === side3 ) {
+console.log ('Isosceles triangle')
+}
+else {
+  console.log('Scalene triangle')
+}
+  
