40476219193082632eee40a29e89a87ef3071cd5
Ayomide Afolabi <aafolabi@smartterm.io>
2025-03-27 14:00:04 +0100

    loop&array

---
+++ arrayassigment.js
@@ -0,0 +1,5 @@
+const friends = ['Tolulope','Aderinola','Olalekan','Damilola','Oluwakorede']
+
+console.log(friends)
+
+console.log(friends[0])
---
+++ loopassignment.js
@@ -0,0 +1,2 @@
+for (let number = 1; number <= 20 ; number +=2)
+  console.log(number)
